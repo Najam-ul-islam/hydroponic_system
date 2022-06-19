fe7d949d1bf49a7b7a694c869f78d1964d53cced
Muhammad Najam Ul Islam <26120402+Najam-ul-islam@users.noreply.github.com>
2022-06-19 14:34:36 +0500

    get request added

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ model = loaded_model = joblib.load("model_final.sav")
 app = Flask(__name__)
 
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=["GET"])
 def predict():
-    if request.method == 'POST':
-        ppm = request.form['PPM']
-        wl = request.form['Water Level']
-        tmp = request.form['Temp']
-        hum = request.form['Humidity']
-        ldr = request.form['LDR']
+    if request.method == 'GET':
+        ppm = request.args.get('PPM')
+        wl = request.args.get('Water_Level')
+        tmp = request.args.get('Temp')
+        hum = request.args.get('Humidity')
+        ldr = request.args.get('LDR')
         df = pd.DataFrame([[ppm, wl, tmp, hum, ldr]], columns=[
-                          'PPM', 'Water Level', 'Temp', 'Humidity', 'LDR'], index=['input'], dtype=np.float64)
+                          'PPM', 'Water_Level', 'Temp', 'Humidity', 'LDR'], index=['input'], dtype=np.float64)
         prediction = model.predict(df)[0]
-        original_input = {'PPM': ppm, 'Water Level': wl,
-                          'Temp': tmp, 'Humidity': hum, 'LDR': ldr, 'Result': prediction}
+        original_input = {'PPM': ppm, 'Water_Level': wl,
+                          'Temp': tmp, 'Humidity': hum, 'LDR': ldr, 'Label': prediction}
     return jsonify(original_input)
 
 
